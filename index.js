fb6e665ede51c4d36cb7d60792c65142d39a35d6
Elfried Alceo Tohouegnon BOTON <boton.alceo@gmail.com>
2024-12-23 08:34:51 +0000

    modification

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const app = express();
 
-// Middleware pour les requêtes JSON
-app.use(express.json());
-
-// Point d'entrée API
 app.get('/api/whoami', (req, res) => {
-  const ipaddress = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const ipaddress = req.ip;
   const language = req.headers['accept-language'];
   const software = req.headers['user-agent'];
-
+  
   res.json({
     ipaddress,
     language,
-    software
+    software,
   });
 });
 
-// Port d'écoute
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
